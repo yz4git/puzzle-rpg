8c93d389344f626c1912a5feb717384c405c0696
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-27 02:27:10 +0900

    Route game through graphics v2 layer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import PuzzleRPGEnhanced from "./PuzzleRPGEnhanced";
+import PuzzleRPGGraphicsV2 from "./PuzzleRPGGraphicsV2";
 import ServiceWorkerRegistration from "./ServiceWorkerRegistration";
 
 export default function Page() {
   return (
     <>
       <ServiceWorkerRegistration />
-      <PuzzleRPGEnhanced />
+      <PuzzleRPGGraphicsV2 />
     </>
   );
 }
