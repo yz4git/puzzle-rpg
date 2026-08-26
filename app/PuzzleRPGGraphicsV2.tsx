85fe59e0c530fe565c4642585eaaef23c201a80d
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-27 02:30:21 +0900

    Activate stage background system

--- app/PuzzleRPGGraphicsV2.tsx
+++ app/PuzzleRPGGraphicsV2.tsx
@@ -5,10 +5,14 @@ import skin from "./PuzzleRPGGraphicsV2.module.css";
 import enemyStage from "./EnemyStageV2.module.css";
 import orbArt from "./OrbArtV2.module.css";
 import battleFx from "./BattleFXV2.module.css";
+import stageBackground from "./StageBackgroundV2.module.css";
 
 export default function PuzzleRPGGraphicsV2() {
   return (
-    <div className={`${skin.root} ${enemyStage.root} ${orbArt.root} ${battleFx.root}`} data-graphics-version="2">
+    <div
+      className={`${skin.root} ${enemyStage.root} ${orbArt.root} ${battleFx.root} ${stageBackground.root}`}
+      data-graphics-version="2"
+    >
       <div className={skin.backdropGrid} aria-hidden="true" />
       <div className={skin.scanlines} aria-hidden="true" />
       <PuzzleRPGEnhanced />
