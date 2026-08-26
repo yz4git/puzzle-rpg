5c3d18582a9447b0356e3a269ee9efeee74d5b33
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-27 02:26:37 +0900

    Start graphics update with UI Skin v2

---
+++ app/PuzzleRPGGraphicsV2.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import PuzzleRPGEnhanced from "./PuzzleRPGEnhanced";
+import skin from "./PuzzleRPGGraphicsV2.module.css";
+
+export default function PuzzleRPGGraphicsV2() {
+  return (
+    <div className={skin.root} data-graphics-version="2">
+      <div className={skin.backdropGrid} aria-hidden="true" />
+      <div className={skin.scanlines} aria-hidden="true" />
+      <PuzzleRPGEnhanced />
+    </div>
+  );
+}
