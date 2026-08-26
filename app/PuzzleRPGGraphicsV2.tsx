ed4312cd2074d2991ff285bde723ea4023f11bd4
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-27 02:28:18 +0900

    Activate Enemy Stage v2

--- app/PuzzleRPGGraphicsV2.tsx
+++ app/PuzzleRPGGraphicsV2.tsx
@@ -2,10 +2,11 @@
 
 import PuzzleRPGEnhanced from "./PuzzleRPGEnhanced";
 import skin from "./PuzzleRPGGraphicsV2.module.css";
+import enemyStage from "./EnemyStageV2.module.css";
 
 export default function PuzzleRPGGraphicsV2() {
   return (
-    <div className={skin.root} data-graphics-version="2">
+    <div className={`${skin.root} ${enemyStage.root}`} data-graphics-version="2">
       <div className={skin.backdropGrid} aria-hidden="true" />
       <div className={skin.scanlines} aria-hidden="true" />
       <PuzzleRPGEnhanced />
