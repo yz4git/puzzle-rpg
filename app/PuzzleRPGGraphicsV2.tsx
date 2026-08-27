6189aac78e29ae567d88ec7976fcd70ab31182a4
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-27 09:56:12 +0900

    Apply first-frame drop icon fix

--- app/PuzzleRPGGraphicsV2.tsx
+++ app/PuzzleRPGGraphicsV2.tsx
@@ -4,13 +4,14 @@ import PuzzleRPGEnhanced from "./PuzzleRPGEnhanced";
 import skin from "./PuzzleRPGGraphicsV2.module.css";
 import enemyStage from "./EnemyStageV2.module.css";
 import orbArt from "./OrbArtV2.module.css";
+import dropSpriteFix from "./DropSpriteIconFix.module.css";
 import battleFx from "./BattleFXV2.module.css";
 import stageBackground from "./StageBackgroundV2.module.css";
 
 export default function PuzzleRPGGraphicsV2() {
   return (
     <div
-      className={`${skin.root} ${enemyStage.root} ${orbArt.root} ${battleFx.root} ${stageBackground.root}`}
+      className={`${skin.root} ${enemyStage.root} ${orbArt.root} ${dropSpriteFix.root} ${battleFx.root} ${stageBackground.root}`}
       data-graphics-version="2"
     >
       <div className={skin.backdropGrid} aria-hidden="true" />
